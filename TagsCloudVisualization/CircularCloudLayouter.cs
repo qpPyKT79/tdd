e6affc24161e3fd574c60815703dbe303db832dd
capitanbanana <d.lukshto@gmail.com>
2017-11-03 20:14:57 +0500

    TagCloudVisualization project added

---
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayouter
+    {
+        CircularCloudLayouter(Point center) { }
+
+        Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [TestFixture]
+    public class CircularCloudLayouter_Should
+    {
+        [Test]
+        public void DoSomething_WhenSomething()
+        {
+
+        }
+    }
+}
